fed61164379b355b0c004d31655628458b374fb3
jamesxv7 <jamesxv7@gmail.com>
2015-12-29 02:25:27 -0400

    Minor changes

--- debesalgo/Controllers/HomeController.cs
+++ debesalgo/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace debesalgo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact page.";
 
             return View();
         }
