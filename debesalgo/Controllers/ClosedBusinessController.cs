253cbcb62b37c5ac2fe430fb171f8411a389bce5
arrancapr <arrancaprcorp@gmail.com>
2016-01-10 22:30:34 -0500

    Grabbing the tags from the Db and placing them out there.

--- debesalgo/Controllers/ClosedBusinessController.cs
+++ debesalgo/Controllers/ClosedBusinessController.cs
@@ -6,16 +6,27 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace debesalgo.Controllers
-{    
+{
+    public class ClosedBusinessResponse
+    {
+        public List<ClosedBusiness> Businesses { set; get; }
+        public List<string> TownTags { set; get; }
+    }
+
     public class ClosedBusinessController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: ClosedBusiness
         public ActionResult Index()
         {
-            var listOfItems = db.ClosedBusinesses.ToList();            
-
-            return Json(listOfItems, JsonRequestBehavior.AllowGet);
+            var listOfItems = db.ClosedBusinesses.ToList();
+            var tags = db.Tags.Where(s=>s.Type == "Town").Select(s=>s.Name).Distinct().ToList();
+            var response = new ClosedBusinessResponse
+            {
+                Businesses = listOfItems,
+                TownTags = tags
+            };
+            return Json(response, JsonRequestBehavior.AllowGet);
         }        
     }
 }
--- debesalgo/Scripts/Models/ClosedBusiness.js
+++ debesalgo/Scripts/Models/ClosedBusiness.js
@@ -4,7 +4,7 @@ $.get("/ClosedBusiness", function (data) {
     function ClosedBusinessListModel()
     {
         var self = this;
-        self.List = ko.mapping.fromJS(data);
+        self.Data = ko.mapping.fromJS(data);
     }   
     ko.applyBindings(new ClosedBusinessListModel);
 });
