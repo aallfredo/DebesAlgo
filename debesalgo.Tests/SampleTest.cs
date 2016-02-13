216a7ac2d165cb92776f73ef6d902c4f82e456bd
arrancapr <arrancaprcorp@gmail.com>
2016-02-12 23:00:26 -0500

    Travis Setup

---
+++ debesalgo.Tests/SampleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using debesalgo;
+using debesalgo.Controllers;
+using NUnit.Framework;
+namespace debesalgo.Tests
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void Empty()
+        {
+            //Sample test for those joining the project.
+            Assert.Pass();
+        }
+    }
+}
